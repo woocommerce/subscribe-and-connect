0e63094befca8c4098cad5d22f573a061561b7ef
Matty <hello@matty.co.za>
2014-03-14 20:04:16 +0200

    Adds the initial layers for the WooFramework importer screen. UX and UI elements pending.

--- classes/class-subscribe-and-connect-admin.php
+++ classes/class-subscribe-and-connect-admin.php
@@ -9,6 +9,14 @@ class Subscribe_And_Connect_Admin {
 	private $_hook;
 
 	/**
+	 * The hook token for the importer screen.
+	 * @access  private
+	 * @var     string
+	 * @since   1.0.0
+	 */
+	private $_importer_screen_hook;
+
+	/**
 	 * The token used to encapsulate this plugin.
 	 * @access  private
 	 * @var     string
@@ -90,7 +98,171 @@ class Subscribe_And_Connect_Admin {
 		// Enqueue our registered scripts and styles on our own admin screen by default.
 		add_action( 'admin_print_scripts-' . $this->_hook, array( $this, 'enqueue_scripts' ) );
 		add_action( 'admin_print_styles-' . $this->_hook, array( $this, 'enqueue_styles' ) );
+
+		$this->_importer_screen_hook = add_submenu_page( 'tools.php', __( 'Subscribe & Connect WooFramework Importer', 'subscribe-and-connect' ), __( 'Subscribe & Connect Importer', 'subscribe-and-connect' ), 'manage_options', 'subscribe-and-connect-wf-importer', array( $this, 'importer_screen' ) );
+		remove_submenu_page( 'tools.php', 'subscribe-and-connect-wf-importer' );
+
+		add_action( 'load-' . $this->_importer_screen_hook, array( $this, 'maybe_process_imports' ) );
 	} // End register_settings_screen()
+
+	/**
+	 * Process the form on the importer screen.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function maybe_process_imports () {
+		if ( ! isset( $_POST ) || 0 >= count( $_POST ) ) return;
+		check_admin_referer( 'subscribe-and-connect-importer', 'subscribe-and-connect-importer' );
+		if ( ! current_user_can( 'manage_options' ) ) wp_die( __( 'Cheatin&#8217; uh?' ) );
+
+		global $subscribe_and_connect;
+		$subscribe_and_connect->setup_settings();
+		$settings = $subscribe_and_connect->get_settings();
+
+		$key_transforms = Subscribe_And_Connect_Utils::get_wf_to_sc_key_transforms();
+		$status = 'false';
+
+		$general = $settings['general'];
+		$connect = $settings['connect'];
+		$display = $settings['display'];
+
+		$keys_to_transform = (array)$_POST['sc_fields_to_import'];
+
+		// Add networks.
+		$networks = array();
+		if ( 0 < count( $keys_to_transform ) ) {
+			foreach ( $keys_to_transform as $k => $v ) {
+				if ( ! in_array( $v, $key_transforms ) ) {
+					$key = str_replace( 'woo_connect_', '', $v );
+					if ( 'googleplus' == $key ) $key = 'google_plus'; // One caveat. :)
+					$networks[$key] = $v;
+				}
+			}
+		}
+
+		$key_transforms = array_merge( $key_transforms, $networks );
+
+		if ( 0 < count( $key_transforms ) ) {
+			foreach ( $key_transforms as $k => $v ) {
+				// Check if the setting is in the first section.
+				if ( isset( $general[$k] ) ) $general[$k] = get_option( $v );
+
+				// Check if the setting is in the second section.
+				if ( isset( $connect[$k] ) ) $connect[$k] = get_option( $v );
+
+				// Check if the setting is in the networks sub-section.
+				if ( isset( $connect['networks'][$k] ) ) $connect['networks'][$k]['url'] = esc_url( get_option( $v ) );
+
+				// Check if the setting is in the third section.
+				if ( isset( $display[$k] ) ) $display[$k] = get_option( $v );
+			}
+		}
+
+		// Fix a quick caveat, with a boolean value.
+		if ( isset( $connect['rss'] ) ) {
+			if ( 'false' == $connect['rss'] ) {
+				$connect['rss'] = false;
+			} else {
+				$connect['rss'] = true;
+			}
+		}
+
+		$statuses = array();
+		$statuses['general'] = update_option( 'subscribe-and-connect-subscribe', $general );
+		$statuses['connect'] = update_option( 'subscribe-and-connect-connect', $connect );
+		$statuses['display'] = update_option( 'subscribe-and-connect-integration', $display );
+
+		if ( ! in_array( false, $statuses ) ) $status = 'true';
+
+		wp_safe_redirect( add_query_arg( 'updated', urlencode( $status ), add_query_arg( 'page', 'subscribe-and-connect-wf-importer', admin_url( 'tools.php' ) ) ) );
+		exit;
+	} // End maybe_process_imports()
+
+	/**
+	 * The importer screen markup.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function importer_screen () {
+		global $subscribe_and_connect;
+		$key_transforms = Subscribe_And_Connect_Utils::get_wf_to_sc_key_transforms();
+		$settings = $subscribe_and_connect->get_settings();
+
+		// Prepare an array of keys to check for.
+		$keys = array();
+
+		// Apply the key transforms detected.
+		if ( 0 < count( $key_transforms ) ) {
+			$keys = array_merge( $keys, array_values( $key_transforms ) );
+		}
+
+		// Add any networks to be checked.
+		if ( isset( $settings['connect']['networks'] ) && 0 < count( $settings['connect']['networks'] ) ) {
+			$prefix = 'woo_connect_';
+			foreach ( $settings['connect']['networks'] as $k => $v ) {
+				if ( ! in_array( $prefix . $k, $keys ) ) {
+					if ( 'google_plus' == $k ) $k = 'googleplus'; // A small caveat to match how the option looks in the WooFramework.
+					$keys[] = $prefix . $k;
+				}
+			}
+		}
+
+		$keys = Subscribe_And_Connect_Utils::filter_existing_keys( $keys );
+?>
+<div class="wrap">
+<?php screen_icon(); ?>
+<h2><?php _e( 'Subscribe & Connect WooFramework Importer', 'subscribe-and-connect' ); ?></h2>
+<form action="" method="post">
+<?php wp_nonce_field( 'subscribe-and-connect-importer', 'subscribe-and-connect-importer' ); ?>
+<?php
+do_action( 'subscribe_and_connect_wf_importer_before' );
+
+if ( 0 >= count( $keys ) ) {
+	_e( 'No existing Subscribe & Connect data, from your WooThemes theme, was found. You\'re good to go!', 'subscribe-and-connect' );
+} else {
+	$key_labels = Subscribe_And_Connect_Utils::get_wf_key_labels();
+?>
+<table class="wp-list-table widefat fixed" style="max-width: 500px;">
+<tbody>
+<?php
+	$counter = 0;
+	foreach ( $keys as $k => $v ) {
+		$counter++;
+		$class = 'alternate';
+		if ( 0 == $counter % 2 ) $class = '';
+		$label = $k;
+		if ( isset( $key_labels[$k] ) && '' != $key_labels[$k] ) $label = $key_labels[$k];
+?>
+	<tr valign="top" class="<?php echo esc_attr( $class ); ?>">
+		<td><input type="checkbox" value="<?php echo esc_attr( $k ); ?>" name="sc_fields_to_import[]" /></td>
+		<td><?php echo esc_html( $label ) . '<br /><code><small>(' . esc_attr( $k ) . ')</small></code>'; ?></td>
+		<td><?php echo esc_html( $v ); ?></td>
+	</tr>
+<?php
+	}
+?>
+</tbody>
+<thead>
+	<tr>
+		<th style="width: 25px;">&nbsp;</th>
+		<th><?php _e( 'Title', 'subscribe-and-connect' ); ?></th>
+		<th><?php _e( 'Stored Value', 'subscribe-and-connect' ); ?></th>
+	</tr>
+</thead>
+</table>
+<?php
+submit_button( __( 'Import Settings', 'subscribe-and-connect' ) );
+}
+
+do_action( 'subscribe_and_connect_wf_importer_after' );
+?>
+</form>
+</div><!--/.wrap-->
+<?php
+
+	} // End importer_screen()
 
 	/**
 	 * The settings screen markup.
--- classes/class-subscribe-and-connect-utils.php
+++ classes/class-subscribe-and-connect-utils.php
@@ -1,6 +1,73 @@
 <?php
 class Subscribe_And_Connect_Utils {
 	/**
+	 * Check if a given array of keys each exist in the wp_options table. If so, return them and their value.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  array key => value pairs of existing keys and data.
+	 */
+	public static function filter_existing_keys ( $keys ) {
+		$response = array();
+		if ( is_array( $keys ) && 0 < count( $keys ) ) {
+			foreach ( $keys as $k => $v ) {
+				$maybe_value = get_option( esc_attr( $v ) );
+				if ( ! is_null( $maybe_value ) && '' != $maybe_value ) {
+					$response[$v] = $maybe_value;
+				}
+			}
+		}
+		unset( $maybe_value );
+		return $response;
+	} // End filter_existing_keys()
+
+	/**
+	 * Get an array of labels for the WooFramework setting keys.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  array key => value pairs of keys and labels.
+	 */
+	public static function get_wf_key_labels () {
+		// Social networks are handled separately via an automated check.
+		return (array)apply_filters( 'subscribe_and_connect_wf_key_labels', array(
+			'woo_connect_title' => __( 'The Title', 'subscribe-and-connect' ),
+			'woo_connect_content' => __( 'The Content', 'subscribe-and-connect' ),
+			'woo_connect_rss' => __( 'Display an RSS icon', 'subscribe-and-connect' ),
+			'woo_connect_newsletter_id' => __( 'Feedburner ID', 'subscribe-and-connect' ),
+			'woo_connect_mailchimp_list_url' => __( 'Mailchimp List Subscription URL', 'subscribe-and-connect' ),
+			'woo_connect_twitter' => __( 'Twitter', 'subscribe-and-connect' ),
+			'woo_connect_facebook' => __( 'Facebook', 'subscribe-and-connect' ),
+			'woo_connect_pinterest' => __( 'Pinterest', 'subscribe-and-connect' ),
+			'woo_connect_youtube' => __( 'YouTube', 'subscribe-and-connect' ),
+			'woo_connect_instagram' => __( 'Instagram', 'subscribe-and-connect' ),
+			'woo_connect_flickr' => __( 'Flickr', 'subscribe-and-connect' ),
+			'woo_connect_googleplus' => __( 'Google +', 'subscribe-and-connect' ),
+			'woo_connect_linkedin' => __( 'LinkedIn', 'subscribe-and-connect' ),
+			'woo_connect_vimeo' => __( 'Vimeo', 'subscribe-and-connect' ),
+			'woo_connect_tumblr' => __( 'Tumblr', 'subscribe-and-connect' ),
+			'woo_connect_dribbble' => __( 'Dribbble', 'subscribe-and-connect' ),
+			'woo_connect_appdotnet' => __( 'App.net', 'subscribe-and-connect' ),
+			'woo_connect_github' => __( 'Github', 'subscribe-and-connect' )
+			) );
+	} // End get_wf_key_labels()
+
+	/**
+	 * Get an array of key transforms for the importer.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  array key => value pairs of key transforms.
+	 */
+	public static function get_wf_to_sc_key_transforms () {
+		// Social networks are handled separately via an automated check.
+		return (array)apply_filters( 'subscribe_and_connect_wf_to_sc_key_transforms', array(
+			'title' => 'woo_connect_title',
+			'text' => 'woo_connect_content',
+			'rss' => 'woo_connect_rss',
+			'newsletter_service_id' => 'woo_connect_newsletter_id',
+			'newsletter_mail_chimp_list_subscription_url' => 'woo_connect_mailchimp_list_url'
+			) );
+	} // End get_wf_to_sc_key_transforms()
+
+	/**
 	 * Get an array of supported networks.
 	 * @access  public
 	 * @since   1.0.0
